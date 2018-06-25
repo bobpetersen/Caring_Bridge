cb49482d60292379fc545212976c85ecc20fa920
Jason Phan <Phanx091@umn.edu>
2018-06-25 13:38:31 -0500

    merge conflict

--- src/components/AdminPage/AdminView.js
+++ src/components/AdminPage/AdminView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Nav from '../../components/Nav/Nav';
 
+
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
 const mapStateToProps = state => ({
