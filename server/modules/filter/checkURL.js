ec62e83868c3af2eff2c1a17cf64535b4607e22f
llenk <ellenkeal@gmail.com>
2018-06-27 10:54:45 -0500

    check for urls

---
+++ server/modules/filter/checkURL.js
@@ -0,0 +1,26 @@
+// checks for a URL in a piece of text
+// will return true if there is a url in the text
+// parameter is the text to check
+
+// checks performed: checking for 'http' in the string
+// checking for .com, .de, .cn, .net, .uk, .in at end of words
+// if there is a gofundme or youcaring link, it will not be flagged
+
+const checkURL = text => {
+  // don't flag gofundme or youcaring
+  if (/[gofundme|youcaring]/.test(text)) {
+    return false;
+  }
+  // check for http in text
+  else if (/http/.test(text)) {
+    return true;
+  } 
+  // check for popular domain endings
+  else if (/[.com|.de|.cn|.net|.uk|.in]\b/.test(text)) {
+    return true;
+  }
+  // if haven't found anything, return false
+  else {
+    return false;
+  }
+}
