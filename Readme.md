0135e68a1a60b07a573f57110c8670a7c007428f
llenk <ellenkeal@gmail.com>
2018-06-29 16:11:28 -0500

    lukes little fixes

--- Readme.md
+++ Readme.md
@@ -6,3 +6,8 @@
 - ** Bob Petersen
 - ** Jason Phan
 - ** Emily Kay
+
+## Maintenance 
+
+### Countries
+To add/delete countries from the blocklist, go to the file at server/modules/filter/countries.js, and follow the instructions in the comments. 
--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -2,38 +2,26 @@ const router = require('express').Router();
 const Profile = require('../models/Profile');
 
 // returns an array of profile objects
-router.get('/', (req, res) => {
-    if (req.isAuthenticated() && req.user.is_active) {
-
-        Profile.find({ isFlagged: true })
-            .then((results) => {
-                res.send(results);
-            })
-            .catch((error) => {
-                console.log(error);
-                res.sendStatus(500);
-            })
-
-    }
-    else {
-        res.sendStatus(403);
-    }
+router.get('/', rejectUnauthenticated, (req, res) => {
+    Profile.find({ isFlagged: true })
+        .then((results) => {
+            res.send(results);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
 });
 
 // takes in 'status' param with value 'reset', 'spam', or 'notSpam'
 // takes in body the _id of the profile
-router.put('/:status', (req, res) => {
-    if (req.isAuthenticated() && req.user.is_active) {
+router.put('/:status', rejectUnauthenticated, (req, res) => {
 
-    }
-    else {
-        res.sendStatus(403);
-    }
 });
 
 //FOR DEVELOPMENT ONLY
 // requires profile object
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
     Profile.create(req.body)
         .then(() => {
             res.sendStatus(201);
@@ -41,7 +29,7 @@ router.post('/', (req, res) => {
         .catch((error) => {
             console.log(error);
             res.sendStatus(500);
-        })
+        });
 });
 
 module.exports = router; 
