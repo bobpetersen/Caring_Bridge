620445946b4fc487a5cdc3ed5cb5881772021771
Jason Phan <Phanx091@umn.edu>
2018-07-02 18:14:29 -0500

    update on site

--- src/redux/sagas/siteSaga.js
+++ src/redux/sagas/siteSaga.js
@@ -1,34 +1,53 @@
-import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import { takeLatest, put as dispatch } from 'redux-saga/effects';
+import { SITE_ACTIONS } from '../actions/siteActions';
+
+const host = "http://localhost:5000/";
 
 function* getSites() {
   try {
-    yield put({
-      type: FETCH_SITES
-    });
-    const sites = yield callSites();
-    yield put({
-      type: SET_SITE_STATUS,
-      sites,
-    });
-  }catch (error) {
-  console.log(error);
+    const sites = yield callSite();
+      yield put({
+        type: SITE_ACTIONS.SET_SITES,
+        payload: sites,
+      });
+      yield put({
+        type: SITE_ACTIONS.FETCH_SITES,
+        sites,
+      });
+  } catch (error) {
+  console.log(`error on siteSaga: ${error}`);
   };
 }
 
 // set status of site
 // include action.payload with 'reset', 'spam', or 'notSpam'
 function* setSiteStatus(action) {
+  try {
+    const setSite = yield call(axios.put, `${host}/status${action.payload.site.id}`,
+    action.payload.site, config)
+    
+    yield put({
+      type: SITE_ACTIONS.SET_SITE_STATUS,
+      payload: setSite,
+
+    })
+  }
+
+
+
 yield call(axios.put, '/:status', {
   reset: action.payload,
   spam: action.payload,
   notSpam: action.payload,
 })
-yield put({ type: 'FETCH_SITES' });
+yield put({ 
+  type: 'FETCH_SITES' 
+});
 }
 
 function* scanSaga() {
-  yield takeEvery('FETCH_SITES', getSites);
-  yield takeEvery('SET_SITE_STATUS', setSiteStatus);
+  yield takeEvery(SITE_ACTIONS.FETCH_SITES, getSites);
+  yield takeEvery(SITE_ACTIONS.SET_SITE_STATUS, setSiteStatus);
 }
 
 export default scanSaga;
