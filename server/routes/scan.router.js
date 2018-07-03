0e23cee2eabaf34d4506c0384e119f728fd7040b
llenk <ellenkeal@gmail.com>
2018-07-03 12:30:10 -0500

    route

--- server/routes/scan.router.js
+++ server/routes/scan.router.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
 // returns objects with lastScannedDate (formatted string) sitesScanned (number) 
+// just returns most recent scan
 router.get('/',(req, res) => {
-
+  Scan.find({}).sort({scanTime: -1}).limit(1)
+  .then(response => {
+    res.send(response);
+  }).catch(error => {
+    console.log(error);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router; 
