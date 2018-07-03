c6ece4b01b58627dd4c0ce72928662b5f884a7c8
Jason Phan <Phanx091@umn.edu>
2018-07-03 14:33:36 -0500

    update on ScanDashboard with object variables, sitesScanned and ScanTime

--- src/components/DashboardPage/ScanDashboardData.js
+++ src/components/DashboardPage/ScanDashboardData.js
@@ -3,21 +3,20 @@ import { connect } from "react-redux";
 import { SCAN_ACTIONS } from '../../redux/actions/scanActions';
 
 const mapStateToProps = reduxState => ({
-    reduxState
+    siteReducer: reduxState.siteReducer
 });
 class ScanDashboardData extends Component {
     componentDidMount() {
         this.props.dispatch({ type: SCAN_ACTIONS.FETCH_SCAN});
     }
     render() {
-    
     return (
       <div>
           {/* site router not yet defined, set default object property as, 'siteScan' until it's declared  */}
-          {/* Sites Scanned : {this.props.reduxState.siteScan} */}
+          Sites Scanned : {this.props.siteReducer.sitesScanned}
           <br />
           {/* site router not yet defined, set default object property as 'lastScan' until it's declared  */}
-          {/* Last Scan Run : {this.props.reduxState.lastScan} */}
+          Last Scan Run : {this.props.siteReducer.scanTime}
       </div>
     );
   }
