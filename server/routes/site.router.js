b3876d23620502290dc98d4298a2305becd42cd1
Rory Ivance <roryvance@gmail.com>
2018-07-05 09:11:17 -0500

    fixed?

--- server/routes/site.router.js
+++ server/routes/site.router.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 });
 
 // takes in 'status' param with value 'spam', or 'safe'
-//  takes in body with id of profile, reason (array of reasons why flagged), and auditedBy (employee id string)
+//  takes in body with id, reason (array of reasons why flagged)
 router.put('/:status', (req, res) => {
     let statusUpdate = req.params.status;
     if (statusUpdate === 'spam') {
@@ -25,7 +25,7 @@ router.put('/:status', (req, res) => {
                 flagged: false,
                 reason: req.body.reason,
                 result: statusUpdate,
-                auditedBy: req.body.auditedBy,
+                auditedBy: req.user.username,
             }
         })
             .then(() => {
