66c7d6b3fac37fcce05e25f0ef7eda6ec926c5bf
Bob Petersen <33petersen@gmail.com>
2018-07-06 15:18:09 -0500

    finished profile data page

--- src/components/ProfilePage/ProfileData.js
+++ src/components/ProfilePage/ProfileData.js
@@ -1,49 +1,17 @@
 import React, { Component } from 'react';
+import ProfileTableBody from './SiteTableBody';
 
 class ProfileData extends Component {
-
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
     
     return (
-      <div>
-          <h2>Profile Data</h2>
-        <table>
-          <tbody>
-            <tr>
-              <th>User</th>
-              <th>User ID</th>
-              <th>User Email</th>
-              <th>User Created</th>
-              <th>Reasons</th>
-            </tr>
-              <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <h2>Last Three Profiles Processed</h2>
-        <table>
-          <tbody>
-            <tr>
-              <th>User</th>
-              <th>User ID</th>
-              <th>User Email</th>
-              <th>User Created</th>
-              <th>Reasons</th>
-            </tr>
-              <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              </tr>
-          </tbody>
-        </table>
+           <div>
+        <h2>Filter Results</h2>
+        <ProfileTableBody />
       </div>
     );
   }
